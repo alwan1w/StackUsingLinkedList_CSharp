c9caf773211b1d8d636362a9d0a7f71b6fad41fa
Alwan Fauzi <alwan.fauzi.ft21@mail.umy.ac.id>
2022-11-30 15:53:35 +0700

    add method pop

--- StackUsingLinkedList_CSharp/Program.cs
+++ StackUsingLinkedList_CSharp/Program.cs
@@ -43,5 +43,11 @@ namespace StackUsingLinkedList_CSharp
             top = fresh;
             Console.WriteLine("\n" + element + " pushed.");
         }
+
+        public void pop()
+        {
+            Console.WriteLine("\nThe popped element is: " + top.info);
+            top = top.Next; //Make top point to the next node in sequence
+        }
     }
 }
